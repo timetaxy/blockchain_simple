94fb434ed6c7a71a5c7c9d94364147b460a7da91
lsh <fantasyz7@naver.com>
2021-11-28 22:44:47 +0900

    sign implemented

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,10 +1,31 @@
 `use strict`;
 const SHA256 = require("crypto-js/sha256");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 class Tx {
   constructor(fromAddr, toAddr, amount) {
     this.fromAddr = fromAddr;
     this.toAddr = toAddr;
     this.amount = amount;
+  }
+  calculatedHash() {
+    return SHA256(this.fromAddr + this.toAddr + this.amount).toString();
+  }
+  signTransaction(signKey) {
+    if (signKey.getPublic("hex") !== this.fromAddr) {
+      throw new Error(`Can't sign, pubKey unmatched`);
+    }
+    const hashTx = this.calculatedHash();
+    const sig = signKey.sign(hashTx, "base64");
+    this.signature = sig.toDER("hex");
+  }
+  isValid() {
+    if (this.fromAddr === null) return true;
+    if (!this.signature || this.signature.length === 0) {
+      throw new Error(`No signature in this tx`);
+    }
+    const pubKey = ec.keyFromPublic(this.fromAddr, "hex");
+    return pubKey.verify(this.calculatedHash(), this.signature);
   }
 }
 class Block {
@@ -38,6 +59,14 @@ class Block {
     }
     console.log(`Block mined:${this.hash}`);
   }
+  hasValidTx() {
+    for (const tx of this.txs) {
+      if (!tx.isValid()) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 class Blockchain {
@@ -60,7 +89,14 @@ class Blockchain {
     this.chain.push(block);
     this.pendingTxs = [new Tx(null, miningRewardAddr, this.miningReward)];
   }
-  createTx(tx) {
+  addTx(tx) {
+    //   createTx(tx) {
+    if (!tx.fromAddr || !tx.toAddr) {
+      throw new Error(`Tx must include from and to addr`);
+    }
+    if (!tx.isValid()) {
+      throw new Error(`Can't add invalid tx`);
+    }
     this.pendingTxs.push(tx);
   }
   getBalance(addr) {
@@ -88,6 +124,9 @@ class Blockchain {
     for (let index = 1; index < this.chain.length; index++) {
       const currBlock = this.chain[index];
       const prevBlock = this.chain[index - 1];
+      if (!currBlock.hasValidTx()) {
+        return false;
+      }
       if (currBlock.hash !== currBlock.calculatedHash()) {
         return false;
       }
