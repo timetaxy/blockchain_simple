05078ed1a38906b085e850a105e22e31038604c2
lsh <fantasyz7@naver.com>
2021-11-29 06:33:11 +0900

    rough test for sign

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 `use strict`;
 const { Blockchain, Tx } = require("./blockchain");
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
+
+const myKey = ec.keyFromPrivate(
+  "a37b802eaad4e2d266364e091fa837bbf544beafff9b34276992cc65963dfba7"
+);
+const myWalletAddr = myKey.getPublic("hex");
+
+let codeCoin = new Blockchain();
+const tx1 = new Tx(myWalletAddr, "public key here", 10);
+tx1.signTransaction(myKey);
+codeCoin.addTx(tx1);
+
+console.log(`Starting mine`);
+console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
+codeCoin.minePendingTxs(myWalletAddr);
+console.log(`balance of mine:${codeCoin.getBalance(myWalletAddr)}`);
+
+//data corruption test
+codeCoin.chain[1].txs[0].amount = 1;
+console.log(`chain valid test : ${codeCoin.isChainValid()}`);
+// console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
+// codeCoin.minePendingTxs("myaddr1");
+
 // const Blockchain = require("./blockchain").Blockchain;
 // const Tx = require("./blockchain").Tx;
 
@@ -27,12 +51,12 @@ const { Blockchain, Tx } = require("./blockchain");
 // }
 
 /*test3 tx*/
-let codeCoin = new Blockchain();
-codeCoin.createTx(new Tx("addr1", "addr2", 100));
-codeCoin.createTx(new Tx("addr2", "addr1", 10));
-console.log(`Starting mine`);
-console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
-codeCoin.minePendingTxs("myaddr1");
-console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
-codeCoin.minePendingTxs("myaddr1");
-console.log(`balance of mine:${codeCoin.getBalance(`myaddr1`)}`);
+// let codeCoin = new Blockchain();
+// codeCoin.createTx(new Tx("addr1", "addr2", 100));
+// codeCoin.createTx(new Tx("addr2", "addr1", 10));
+// console.log(`Starting mine`);
+// console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
+// codeCoin.minePendingTxs("myaddr1");
+// console.log(`now pending tx:${JSON.stringify(codeCoin.pendingTxs)}`);
+// codeCoin.minePendingTxs("myaddr1");
+// console.log(`balance of mine:${codeCoin.getBalance(`myaddr1`)}`);
